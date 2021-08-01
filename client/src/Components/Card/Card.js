aeac78288b142da20d0df9a7759782d941e81c74
Francisco <63214442+frankymz@users.noreply.github.com>
2021-08-01 02:15:10 -0400

    dropdown

--- client/src/Components/Card/Card.js
+++ client/src/Components/Card/Card.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-export default function Card(){
-    
+export default function Card(props) {
+  const data = props.data;
 
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+          maxWidth: "900px",
+          margin: "50px",
+          borderBottom: "1px solid gray",
+          justifyContent:"space-between"
+        }}
+      >
+        <div style={{maxWidth:"600px"}}>
+          <div style={{ fontSize: "25px", marginBottom: "10px" }}>
+            {data.title}
+          </div>
+          <div>{data.description}</div>
+          <div>{data.content}</div>
+          <div style={{ marginTop: "20px" }}>
+            By {data.author} on {data.publishedAt}
+          </div>
+          <Link to={data.url}>{data.url}</Link>
+        </div>
 
-    return(
-
-        <React.Fragment>
-
-            <div>
-                Card Component
-            </div>
-        </React.Fragment>
-    )
+        <div>
+          <img style={{ height: "100px" }} src={data.urlToImage} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
