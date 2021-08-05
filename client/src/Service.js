3ea60bc0ec096dc0521857a39833ffec9b5a0760
Francisco <63214442+frankymz@users.noreply.github.com>
2021-08-05 17:19:24 -0400

    cors

--- client/src/Service.js
+++ client/src/Service.js
@@ -32,7 +32,8 @@ class Service {
       headers: {
         Authorization: `${process.env.REACT_APP_APIKEY}`,
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'Sec-Fetch-Mode': 'no-cors'
       },
     });
   }
