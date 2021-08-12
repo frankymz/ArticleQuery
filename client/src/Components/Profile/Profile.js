3d9ffe607e5187961200cde1d8e9bb7ff14c473e
Francisco <63214442+frankymz@users.noreply.github.com>
2021-08-12 12:28:42 -0400

    gitignore

--- client/src/Components/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Profile() {
-  return (
-    <React.Fragment>
-      <div>Profilee</div>
-    </React.Fragment>
-  );
-}
--- settings.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-
-FLASK_CLIENTSECRET = environ.get('FLASK_CLIENTSECRET')
-FLASK_CLIENTID = environ.get('FLASK_CLIENTID')
-FLASK_SECRET = environ.get('FLASK_SECRET')
